c876db7f27a3148143e152a0402c54a88024b7e2
Ismail0Fokri <fokkismail@gmail.com>
2023-03-26 16:44:24 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-#include<stdio.h>
-/*puts function*/
+#include <stdio.h>
+/**
+ *  main -  program used to print a line with puts function
+*/
 int main(void)
 (
  puts("\"Programming is like building a multilingual puzzle");
- return 0;
+ return (0);
 }
