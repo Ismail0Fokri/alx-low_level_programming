8ceb62418a00449749af95aba7e4520fe9115f18
IsmailElFk <ismailfokri@gmail.com>
2023-04-02 23:32:23 +0200

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+    for (int x=0; x <= 10; x++)
+    {
+        printf("%d", x);
+    }
+
+}
