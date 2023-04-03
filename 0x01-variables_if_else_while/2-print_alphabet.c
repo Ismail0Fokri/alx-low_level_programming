d249d2e9963e93b97c008047553b5d95a16e29e2
IsmailElFk <ismailfokri@gmail.com>
2023-04-03 10:05:52 +0200

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    for (char p = 'a';p <= 'z'; p++)
+        putchar(p);   
+}
